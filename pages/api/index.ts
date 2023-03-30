e291d9149373d8a2cfc8dabd5b2783e828558a0f
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-03-29 22:54:35 -0400

    update expiration date

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -16,7 +16,7 @@ export async function fetchAllClients(
   let page = 1;
   const limit = 49;
 
-  while (page < 12) {
+  while (page < 11) {
     const res = await fetch(
       `https://api.datacapstats.io/api/getVerifiedClientsExtended?page=${page}&limit=${limit}&intervalStartTimestamp=${intervalStartTimestamp}&intervalEndTimestamp=${intervalEndTimestamp}`
     );
@@ -31,7 +31,7 @@ export async function fetchAllClients(
     page++;
 
     //wait 2 seconds before the next request
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 300));
   }
 
   return allClients;
