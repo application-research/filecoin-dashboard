e548d93fe207606601fc33b4e92efd77ed4a3642
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2023-04-18 14:13:57 -0400

    added inprogress banner

--- components/NavbarNew.tsx
+++ components/NavbarNew.tsx
@@ -6,64 +6,60 @@ import TwitterBlackSVG from "./svgs/TwitterBlackSVG";
 
 export default function NavbarNew() {
   return (
-    <div className={styles.body}>
-      <GutterContainer>
-        <div className={styles.navContainer}>
-          <div className={styles.logoContainer}>
-            <a href="https://filecoin.io/" target="_blank">
-              <FilecoinSVG
-                style={{
-                  width: "2.5rem",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              />
-            </a>
-            <p className={styles.filecoinLogo}>Filecoin</p>
-          </div>
-
-          <div className={styles.socialContainer}>
-            <a
-              className={styles.textButtonLink}
-              href="https://protocol.ai/"
-              target="_blank"
-            ></a>
-
-            <div className={styles.socialIconsRow}>
-              <a
-                className={styles.link}
-                href="https://github.com/filecoin-project/filecoin-docs"
-                target="_blank"
-              >
-                <GithubBlackSVG
+    <div>
+      <div className={styles.banner}>
+        <p className={styles.bannerText}>This site is under construction</p>
+      </div>
+      <div className={styles.body}>
+        <GutterContainer>
+          <div className={styles.navContainer}>
+            <div className={styles.logoContainer}>
+              <a href="https://filecoin.io/" target="_blank">
+                <FilecoinSVG
                   style={{
-                    width: "1.5rem",
-                    fill: "var(--color-black)",
-                    color: "var(--color-black)",
+                    width: "2.5rem",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
                   }}
                 />
               </a>
+              <p className={styles.filecoinLogo}>Filecoin</p>
+            </div>
+
+            <div className={styles.socialContainer}>
               <a
-                className={styles.link}
-                href="https://twitter.com/filecoin"
+                className={styles.textButtonLink}
+                href="https://protocol.ai/"
                 target="_blank"
-              >
-                <TwitterBlackSVG
-                  // fill="var(--color-black)"
-                  style={
-                    {
-                      // width: "1.5rem",
-                      // fill: "red",
-                      // color: "var(--color-black)",
-                    }
-                  }
-                />
-              </a>
+              ></a>
+
+              <div className={styles.socialIconsRow}>
+                <a
+                  className={styles.link}
+                  href="https://github.com/filecoin-project/filecoin-docs"
+                  target="_blank"
+                >
+                  <GithubBlackSVG
+                    style={{
+                      width: "1.5rem",
+                      fill: "var(--color-black)",
+                      color: "var(--color-black)",
+                    }}
+                  />
+                </a>
+                <a
+                  className={styles.link}
+                  href="https://twitter.com/filecoin"
+                  target="_blank"
+                >
+                  <TwitterBlackSVG />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </GutterContainer>
+        </GutterContainer>
+      </div>
     </div>
   );
 }
